b248cdb8e51f90a1787c26727aed3745af701918
zevladnit <zevladnit@mail.ru>
2019-05-06 18:22:56 +0300

    described db context and Coordinates Model

---
+++ ModelCoordinates.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GPSCoordinateInfo.SQLiteModel
+{
+    public class ModelContext : DbContext
+    {
+        public DbSet<Coordinate> Coordinates { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=blogging.db");
+        }
+       
+    }
+
+    public class Coordinate
+    {
+        [Key]
+        public int IDCoordinate { get; set; }
+        public float CoordinateX { get; set; }
+        public float CoordinateY { get; set; }
+        public string AddressContent { get; set; }
+
+        public override string ToString()
+        {
+            return $"{IDCoordinate},{CoordinateX},{CoordinateY},{AddressContent}";
+        }
+    }
+}
