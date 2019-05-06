dcc85a689a03b9be7d2440f35f9213a7328ed9c0
zevladnit <zevladnit@mail.ru>
2019-05-06 14:49:17 +0300

    Tested YandexAPI
    received xml and address in coordinates 30.315868, 59.939095 Result: "Россия, Санкт-Петербург, Дворцовая площадь"

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using YandexAPI.Maps;
+namespace GPSCoordinateInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GeoCode geoCode = new GeoCode();
+            string infoXML = geoCode.SearchObject(30.315868, 59.939095);
+            string Adress = geoCode.GetAddress(infoXML);
+            Console.WriteLine(Adress);
+            Console.ReadKey();
+        }
+    }
+}
