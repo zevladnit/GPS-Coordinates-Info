45123a601cd8167bcef70ff3eea93dbb4e5baa3d
zevladnit <zevladnit@mail.ru>
2019-05-08 00:19:59 +0300

    Config Model separated fromCoordinates

---
+++ Models/ModelContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GPSCIService.Models
+{
+    public class ModelContext : DbContext
+    {
+        public DbSet<Coordinate> Coordinates { get; set; }
+        public DbSet<TwoCoordinate> TwoCoordinates { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("coordinate.db");
+        }
+        public ModelContext()
+        {
+        }
+    }
+}
